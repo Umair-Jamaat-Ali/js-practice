64f812e7216c3c8f6e2f920d6a24e59955c63530
Umair-Jamaat-Ali <umairansari758@gmail.com>
2023-03-17 11:40:04 -0700

    js practice

---
+++ script.js
@@ -0,0 +1,52 @@
+// alert ("Thanks for your input");  // alert practice ,, is this some message to the user
+
+ // this type data is string data and is always writes in double , single and ` qoutation 
+
+ var userName = "Umair Jamaat Ali"; 
+ console.log ("userName" , userName)
+ // this type data is call number data and its written always in numbers
+ 
+ 
+ var cellNum = 03450712423 
+ console.log ("Cell #", cellNum)
+ 
+ // this type of data is floating data and its written in dicimal digits
+ var dicimal = 654.987; 
+ console.log ("decimal", dicimal)
+ 
+ ///Math expresitions and operation
+ var total = 2 + 4 - 8 * 8 ;
+ console.log ("Is equal to", total)
+ //sum (+)
+ var sum = 2+7;
+ console.log ("answer is", sum)
+ // subracts (-)
+ var sub = 99 - 56;
+ console.log ("answer is", sub)
+ 
+ // Multiple (*)
+ var multi = 9 * 8;
+ console.log ("answer is", multi)
+ 
+ // Division (/)
+ var divi = 90 / 11;
+ console.log ("answer is", divi)
+ 
+ 
+ 
+  // Solve a problems
+  // Write a program that convert kilograms in grams
+  var kg = prompt("Pleas enter the weight in kg")
+  // (kg * 1000 grams) 
+  var grams = (kg * 1000 );
+ 
+  console.log("Weight in grams", grams)
+ 
+  
+  // Solve a problems
+  // Write a program that convert  in grams
+  var kg = prompt("Pleas enter the weight in kg")
+  // (kg * 1000 grams) 
+  var grams = (kg * 1000 );
+ 
+  console.log("Weight in grams", grams)
